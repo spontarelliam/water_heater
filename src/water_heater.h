d489f8f1bfcfb4cace28b0b25a9a87a21c836665
Adam Spontarelli <spontarelliam@gmail.com>
2022-03-22 18:17:13 -0400

    initial commit

---
+++ src/water_heater.h
@@ -0,0 +1,142 @@
+class FlowMeter{
+public:
+    int PIN;
+    float flow_rate = 0;
+    volatile int flow_frequency; // Measures flow sensor pulsesunsigned
+    unsigned long currentTime;
+    unsigned long cloopTime = millis();
+    volatile int flow_frequency; // Measures flow sensor pulsesunsigned
+
+    void set_pin(int pin){
+        PIN = pin;
+        pinMode(pin, INPUT_PULLUP);
+        attachInterrupt(0, flow, RISING); // Setup Interrupt
+    }
+
+    void flow(){ // Interrupt function
+        flow_frequency++;
+    }
+
+    void calc_flow_rate(){
+        currentTime = millis();
+
+        if(currentTime >= (cloopTime + 1000))
+        {
+            cloopTime = currentTime; // Updates cloopTime
+            // Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min.
+            l_hour = (flow_frequency * 60 / 7.5); // (Pulse frequency x 60 min) / 7.5Q = flowrate in L/hour
+            flow_frequency = 0; // Reset Counter
+            Serial.print(l_hour, DEC); // Print litres/hour
+            Serial.println(" L/hour");
+        }
+    }
+}
+
+
+class Heater {       // The class
+  public:             // Access specifier
+    int PIN;
+    bool on = false;
+    float power; // in watts
+    unsigned int time_on = 0;
+    unsigned int start_time;
+    float temp_history[10] = {0,0,0,0,0,0,0,0,0,0};
+    float rate=1;
+
+    void set_pin(int pin){
+      PIN = pin;
+      pinMode(pin, OUTPUT);
+    }
+
+    void set_power(int setpoint){
+      setpoint = min(setpoint, 255);
+      analogWrite(PIN, setpoint);
+      power = map(setpoint, 0, 255, 0, 5000);
+
+      if (power > 0){
+        // heater just turned on from off
+	if (on == false){
+	  start_time = millis();
+	}
+	on = true;
+      }
+      else{
+	on = false;
+      }
+    }
+
+    void get_state(){
+      delay(1);
+    }
+
+  int get_time_on(){
+    if (on == true){
+      time_on = millis() - start_time;
+    }
+    else{
+      time_on = 0;
+    }
+    return time_on;
+  }
+
+  void append_temp(float temp){
+    for (int i = 0; i<=8; i++){
+      temp_history[i] = temp_history[i+1];
+      //Serial.println(T_history[i]);
+    }
+    temp_history[9] = temp;
+  }
+
+  int get_power(){
+    return power;
+  }
+};
+
+
+
+
+class Thermistor{
+  public:
+  int PIN;
+  long THERMISTORNOMINAL = 120000;
+  int TEMPERATURENOMINAL = 25;
+  int BCOEFFICIENT = 3000; // The beta coefficient of the thermistor (usually 3000-4000)
+  long SERIESRESISTOR = 100000;
+  float temp;
+
+  void set_pin(int pin){
+    PIN = pin;
+  }
+
+  float get_temperature(){
+    int NUMSAMPLES = 5;
+    int samples[NUMSAMPLES];
+    float average = 0;
+    float steinhart;
+    // take N samples in a row, with a slight delay
+    for (int i=0; i< NUMSAMPLES; i++) {
+      samples[i] = 1023-analogRead(PIN);
+      delay(10);
+    }
+    // average all the samples out
+    for (int i=0; i< NUMSAMPLES; i++) {
+      average += samples[i];
+    }
+    average /= NUMSAMPLES;
+
+    // convert the value to resistance
+    average = 1023 / average - 1;
+    average = SERIESRESISTOR / average;
+
+
+    steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
+    steinhart = log(steinhart);                  // ln(R/Ro)
+    steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+    steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+    steinhart = 1.0 / steinhart;                 // Invert
+    steinhart -= 273.15;                         // convert absolute temp to C
+
+    temp = steinhart * 1.8 + 32;
+    return temp;
+  }
+};
